82e25e0b43ec85410df20af8515223b7e9be281c
DustinDEV2more <public@dustin-dm.de>
2021-08-30 20:41:08 +0200

    add Notification to Usersettings

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import Userpage from "./pages/User"
 import Usersettings from "./pages/Usersettings"
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 
+import ReactNotification from 'react-notifications-component'
+import 'react-notifications-component/dist/theme.css'
+
+
 function App() {
   return (
     <Router>
     <div className="App">
       <Header />
+      <ReactNotification />
 
       <Switch>
       <Route exact path="/"><h1>Home</h1></Route>
